67722df753aff53599f6d0cd623ada827d8a6ce7
zhoucy <1049910616@qq.com>
2023-06-27 20:32:02 +0800

    课程计划排序

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,15 +1,20 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.base.exception.ValidationGroups;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
-import com.xuecheng.content.dto.QueryCourseParamsDto;
-import com.xuecheng.content.po.CourseBase;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,17 +28,38 @@ import java.util.List;
 @Api(value = "课程信息管理接口",tags = "课程信息管理接口")
 @RestController //相当于@Controller和responseBody
 public class CourseBaseInfoController {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("程序查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("测试名称");
-        courseBase.setCreateDate(LocalDateTime.now());
-        List<CourseBase> courseBases = new ArrayList();
-        courseBases.add(courseBase);
-        PageResult pageResult = new PageResult<CourseBase>(courseBases,10,1,10);
-        return pageResult;
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+        return courseBasePageResult;
+    }
 
+    @ApiModelProperty("新增课程")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated(ValidationGroups.Insert.class) AddCourseDto addCourseDto) {
+        Long companyId = 1232141425L;
+        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
+
+    }
+
+    @ApiModelProperty("根据课程id查询课程基础信息")
+    @GetMapping("/course/{courseId}")
+    public CourseBaseInfoDto getCourseBase(@PathVariable Long courseId) {
+        CourseBaseInfoDto courseBaseInfo = courseBaseInfoService.getCourseBaseInfo(courseId);
+        return courseBaseInfo;
+
+    }
+    @ApiModelProperty("修改课程基础信息")
+    @PutMapping("/course")
+    public CourseBaseInfoDto modifyCourseBase(@RequestBody @Validated(ValidationGroups.Update.class) EditCourseDto editCourseDto) {
+        Long companyId = 1232141425L;
+        CourseBaseInfoDto courseBaseInfo = courseBaseInfoService.updateCourseBase(companyId,editCourseDto);
+        return courseBaseInfo;
 
     }
 }
+
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMediaMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMediaMapper.java
@@ -3,6 +3,8 @@ package com.xuecheng.content.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.content.model.po.TeachplanMedia;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -11,5 +13,16 @@ import com.xuecheng.content.model.po.TeachplanMedia;
  * @author itcast
  */
 public interface TeachplanMediaMapper extends BaseMapper<TeachplanMedia> {
+
+//    /**
+//     * @description 查询某课程计划的media
+//     * @param teachplanId
+//     * @return com.xuecheng.content.model.po.TeachplanMedia
+//     * @author zcy
+//     * @date 2023/6/27
+//     */
+//
+//    //课程计划查询
+//    public List<TeachplanMedia> selectMedia(Long teachplanId);
 
 }
