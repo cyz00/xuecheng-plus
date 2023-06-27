d44120b5253a10cae7c1c0e97323affd69e432d0
zhoucy <1049910616@qq.com>
2023-06-27 20:32:02 +0800

    课程计划排序

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,7 +1,10 @@
 package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,5 +14,15 @@ import com.xuecheng.content.model.po.Teachplan;
  * @author itcast
  */
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
+    /**
+     * @description 查询某课程的课程计划，组成树型结构
+     * @param courseId
+     * @return com.xuecheng.content.model.dto.TeachplanDto
+     * @author zcy
+     * @date 2023/6/26
+     */
+
+    //课程计划查询
+    public List<TeachplanDto> selectTreeNodes(Long courseId);
 
 }
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,53 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * @description 课程基本信息管理业务接口
+ * @author zcy
+ * @date 2023/6/16
+ * @version 1.0
+*/
+public interface CourseBaseInfoService  {
+
+/**
+ * @description 课程查询接口
+ * @param pageParams 分页参数
+ * @param queryCourseParamsDto 条件条件
+ * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+ * @author zcy
+ * @date 2023
+ */
+    //课程分页查询
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+    /**
+     * @description 添加课程基本信息
+     * @param companyId  教学机构id
+     * @param addCourseDto  课程基本信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author zcy
+     * @date 2023/6/21
+     */
+
+    CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+    /**
+     * @description 根据课程id查询课程信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author zcy
+     * @date 2023/6/25
+     */
+    CourseBaseInfoDto getCourseBaseInfo(Long courseId);
+    /**
+     * @description 修改课程信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author zcy
+     * @date 2023/6/25
+     */
+    CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto editCourseDto);
+}
