c50a6527f8d5400421b754bfebb2b84fb180d977
zhoucy <1049910616@qq.com>
2023-07-10 10:23:03 +0800

    base中的utils类

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/utils/Mp4VideoUtil.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/utils/Mp4VideoUtil.java
@@ -83,13 +83,13 @@ public class Mp4VideoUtil extends VideoUtil {
 
     public static void main(String[] args) throws IOException {
         //ffmpeg的路径
-        String ffmpeg_path = "D:\\soft\\ffmpeg\\ffmpeg.exe";//ffmpeg的安装位置
+        String ffmpeg_path = "F:\\zcy\\java\\software\\ffmpeg\\ffmpeg.exe";//ffmpeg的安装位置
         //源avi视频的路径
-        String video_path = "D:\\develop\\bigfile_test\\nacos01.avi";
+        String video_path = "C:\\Users\\tianlei\\Videos\\out.avi";
         //转换后mp4文件的名称
-        String mp4_name = "nacos01.mp4";
+        String mp4_name = "out.mp4";
         //转换后mp4文件的路径
-        String mp4_path = "D:\\develop\\bigfile_test\\";
+        String mp4_path = "F:\\zcy\\mdia\\video\\bigfile_test\\";
         //创建工具类对象
         Mp4VideoUtil videoUtil = new Mp4VideoUtil(ffmpeg_path,video_path,mp4_name,mp4_path);
         //开始视频转换，成功将返回success
